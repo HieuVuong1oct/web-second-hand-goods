73becd848cd31026563719c2ea70d7fd23ba3c57
hieu <hieuvuong01@gmail.com>
2024-07-31 15:32:58 +0700

    fix: reset token

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -10,16 +10,37 @@ const axiosClient = axios.create({
   withCredentials: true,
 });
 
+const refreshAccessToken = async () => {
+  try {
+    const refreshToken = Cookies.get('refreshToken');
+    if (refreshToken) {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/refresh-token`, { refreshToken });
+      const newAccessToken = response.data.accessToken;
+      Cookies.set('accessToken', newAccessToken);
+      return newAccessToken;
+    }
+  } catch (error) {
+    alert('Lỗi xảy ra')
+  }
+  return null;
+};
+
 axiosClient.interceptors.request.use(
   async (config) => {
-    const accessToken = Cookies.get('accessToken');
+    let accessToken = Cookies.get('accessToken');
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
+    } else {
+      accessToken = await refreshAccessToken();
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
     }
     return config;
   },
   (error) => Promise.reject(error)
 );
+
 axiosClient.interceptors.response.use(
   (response) => {
     if (response && response.data) {
@@ -27,9 +48,26 @@ axiosClient.interceptors.response.use(
     }
     return response;
   },
-  (error) => {
-    throw error;
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const newAccessToken = await refreshAccessToken();
+      if (newAccessToken) {
+        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+        return axiosClient(originalRequest);
+      }
+    }
+    return Promise.reject(error);
   }
 );
+
+const startTokenRefreshInterval = () => {
+  setInterval(async () => {
+    await refreshAccessToken();
+  }, 15 * 60 * 1000); 
+};
+
+startTokenRefreshInterval();
 
 export default axiosClient;
